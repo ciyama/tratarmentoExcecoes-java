503dfbef6de7155f7df7c3ce81b32b821400b910
Celio Iyama <celio.iyama@gmail.com>
2021-07-12 10:35:20 -0300

    Tratamento de exceções solution padrao try-catch - java

--- excecoesPersonalizadas/src/model/entities/Reservation.java
+++ excecoesPersonalizadas/src/model/entities/Reservation.java
@@ -45,18 +45,17 @@ public class Reservation {
 		
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for Update must be future dates";
+			throw new IllegalArgumentException("Reservation dates for Update must be future dates");
 		}
 		if (!checkOut.after(checkIn))	{
-			return "Check-out date must be after check-in date";
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
