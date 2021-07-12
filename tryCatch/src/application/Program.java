357f5086245c0d602f6d2b96eba8c14f9c8cf450
Celio Iyama <celio.iyama@gmail.com>
2021-07-11 22:40:27 -0300

    Estrutura Try-catch - java

--- tryCatch/src/application/Program.java
+++ tryCatch/src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		}
+		catch (java.util.InputMismatchException e) {
+			System.out.println("Input error");
+		}
+			
+			System.out.println("End of program");
+		sc.close();
+	
 	}
 
 }
